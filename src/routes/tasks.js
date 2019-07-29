844b71ec7052e3a86bb5acc61a6073182278b337
Gerardo Muniz <gerardofigueredomuniz@gmail.com>
2019-07-29 12:07:33 -0300

    parcialmente aula 2

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,14 +1,24 @@
 import knex from "../config/knex";
 
-const tasks_route = {
-  method: "GET",
-  path: "/tasks",
-  handler: (request, reply) =>
-    knex
-      .from("tasks")
-      .select("oid", "title", "description")
-      .then(results => reply.response(results))
-      .catch(err => console.log(err))
-};
+const tasks_route = [
+  {
+    method: "GET",
+    path: "/tasks",
+    handler: (request, reply) =>
+      knex
+        .from("tasks")
+        .select("oid", "title", "description")
+        .then(results => reply.response(results))
+        .catch(err => console.log(err))
+  },
+  {
+    method: "POST",
+    path: "/tasks",
+    handler: (request, reply) => ({
+      status: "201",
+      data: "Ok"
+    })
+  }
+];
 
 export default tasks_route;
